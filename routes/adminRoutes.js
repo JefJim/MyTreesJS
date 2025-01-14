3015f5d43354ec0b630fd57ba4bdc774637e7af8
JefJim <jefryjimenez2011@gmail.com>
2025-01-14 15:23:00 -0600

    Adding admin/user routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authMiddleware = require("../public/js/auth");
 const adminController = require("../controllers/adminController");
 const speciesController = require("../controllers/speciesController");
+const usersController = require("../controllers/clientController");
 
 
 // Only admin users can access this route
@@ -17,7 +18,18 @@ router.get("/stats", adminController.getDashboardStats);
 router.put("/update/:id", speciesController.updateSpecies);
 router.delete("/species/:id", speciesController.deleteSpecies);
 router.post("/create", speciesController.createSpecies);
-router.get("/list", speciesController.getAllSpecies);
+router.get("/species/list", speciesController.getAllSpecies);
 router.get("/get/:id", speciesController.getSpeciesById);  
+
+//routes for users
+router.get("/users/get/:id", adminController.getUsersById);
+router.get("/users/check-email", adminController.checkEmail);
+
+router.get("/users/list", adminController.getAllUsers);
+router.post("/users/create", adminController.createUser);
+router.put("/users/edit/:id", adminController.updateUser);
+router.delete("/users/delete/:id", adminController.deleteUser);
+
+
 
 module.exports = router;
